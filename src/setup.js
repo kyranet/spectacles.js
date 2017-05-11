d683cf74cf974762fe89658c6351800e18f408aa
Will Nelson <nelson.will@live.com>
2017-05-10 23:49:54 -0700

    remove old file

--- src/setup.js
+++
@@ -1,5 +0,0 @@
-module.exports = (redis, data = {}) =>
-  Promise.all(Object.keys(data).map((k) => {
-    if (data[k] instanceof Object) return redis.hmsetAsync(k, data[k]);
-    return redis.setAsync(k, data[k]);
-  }));
